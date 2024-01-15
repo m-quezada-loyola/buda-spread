98100742a186859521a6f7cc1e87d91ed88f4ddc
Matias Quezada <m.quezada.loyola@gmail.com>
2024-01-15 20:52:04 -0300

    Create config file for Swagger

---
+++ backend/swagger.js
@@ -0,0 +1,37 @@
+const swaggerJsdoc = require("swagger-jsdoc"); 
+const swaggerUi = require("swagger-ui-express");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Buda Spread API",
+      description:
+        "API que entrega los spreads de mercado entre distintos pares de dívisas.",
+      contact: {
+        name: "Matías Quezada",
+        email: "m.quezada.loyola@gmail.com",
+        url: "https://github.com/m-quezada-loyola",
+      },
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000/",
+        description: "Local server",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+module.exports.swaggerDocs = (app, port) => {
+  app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.get("/docs.json", (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.send(swaggerSpec);
+  });
+}
+
+
